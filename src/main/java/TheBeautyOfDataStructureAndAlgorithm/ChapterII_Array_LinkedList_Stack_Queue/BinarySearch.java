341de3b8585d3f44d6f88ad37845a2b635b65c25
wpsone <173897137@qq.com>
2022-10-08 23:10:39 +0800

    数据结构与算法之美-二分查找

--- src/main/java/TheBeautyOfDataStructureAndAlgorithm/ChapterII_Array_LinkedList_Stack_Queue/BinarySearch.java
+++ src/main/java/TheBeautyOfDataStructureAndAlgorithm/ChapterII_Array_LinkedList_Stack_Queue/BinarySearch.java
@@ -33,4 +33,72 @@ public class BinarySearch {
             return bsearchInternally(a, low, mid - 1, value);
         }
     }
+
+    //二分查找变体问题1：查找第一个值等于给定值的元素
+    public int bsearch1(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == 0) || (a[mid - 1] != value)) return mid;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    //二分查找变体问题2：查找最后一个值等于给定值的元素
+    public int bsearch2(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] != value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    //二分查找变体问题3：查找第一个值大于或等于给定值的元素
+    public int bsearch3(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    //二分查找变体问题4：查找最后一个值小于或等于给定值的元素
+    public int bsearch4(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
