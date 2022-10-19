3a644aae293d33520f3501019666cda790be2d3a
wpsone <173897137@qq.com>
2022-10-19 08:14:32 +0800

    数据结构与算法之美-LRU缓存淘汰算法

---
+++ src/main/java/TheBeautyOfDataStructureAndAlgorithm/ChapterII_Array_LinkedList_Stack_Queue/BitMap.java
@@ -0,0 +1,26 @@
+package TheBeautyOfDataStructureAndAlgorithm.ChapterII_Array_LinkedList_Stack_Queue;
+
+//位图
+public class BitMap {
+    private char[] bytes;//在java中，char类型占16bit，即2B
+    private int nbits;
+
+    public BitMap(int nbits) {
+        this.nbits = nbits;
+        this.bytes = new char[nbits/16+1];
+    }
+
+    public void set(int k) {
+        if (k>nbits) return;
+        int byteIndex = k / 16; //存储在数组第几个上
+        int bitIndex = k % 16;  //存储在数组第byteIndex个的第几个二进制位上
+        bytes[byteIndex] |= (1 << bitIndex);
+    }
+
+    public boolean get(int k) {
+        if (k>nbits) return false;
+        int byteIndex = k/16;
+        int bitIndex = k%16;
+        return (bytes[byteIndex] & (1 << bitIndex)) != 0;
+    }
+}
